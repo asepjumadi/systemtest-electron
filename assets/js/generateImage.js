886e9b16cbc1a8502abd75b373c886a91e439f6f
asep jumadi <asepjumadi27@gmail.com>
2020-02-12 08:32:47 +0700

    ok

--- assets/js/generateImage.js
+++ assets/js/generateImage.js
@@ -172,7 +172,7 @@ function printall(quality = 2) {
 
   html2canvas(document.querySelector('#dataTable'), 
                           {scale: quality}
-                   ).then(canvas => {
+              ).then(canvas => {
       let pdf = new jsPDF('p', 'mm', 'a4');
       pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
       pdf.save(filename);
